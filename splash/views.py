870799afed2fa0c710980daffff796c192de27f5
khadija-bit <sumeyahassan34@gmail.com>
2020-05-24 15:49:56 +0300

    Add function for category and location

--- splash/views.py
+++ splash/views.py
@@ -8,4 +8,6 @@ def home(request):
 def location(request):
     return render(request,'all-pictures/location.html')
 
-    
+def category(request):
+    return render(request,'all-pictures/category.html')
+        
