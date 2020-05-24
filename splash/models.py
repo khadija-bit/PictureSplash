db2d342947126a6425490abee604648d9f96c20b
khadija-bit <sumeyahassan34@gmail.com>
2020-05-24 18:00:12 +0300

    Add save function to save Location Category class

--- splash/models.py
+++ splash/models.py
@@ -7,9 +7,14 @@ class Location(models.Model):
     def __str__(self):
         return self.location
 
+    def save_location(self):
+        self.save    
+
 class Category(models.Model):
     category = models.CharField(max_length = 60)
 
     def __str__(self):
         return self.category
-
+    
+    def save_category(self):
+        self.save  
