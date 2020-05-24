96afc2886771ead090f8a26a20f63eb2486b4a0e
khadija-bit <sumeyahassan34@gmail.com>
2020-05-24 15:26:35 +0300

    Added Location class in model method

--- splash/models.py
+++ splash/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-# Create your models here.
-# class Location(models.Model):
-#     location = models.CharField(max_lenght = 60)
+Create your models here.
+class Location(models.Model):
+    location = models.CharField(max_lenght = 60)
 
-#     def __str__(self):
-#         return self.location
+    def __str__(self):
+        return self.location
     
